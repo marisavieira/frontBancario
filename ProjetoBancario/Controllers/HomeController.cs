b2ad9872778b58d99200959d6a6c1b39988f0cff
marisavieira <marisavrst@gmail.com>
2020-03-06 17:23:08 -0300

    Visual Telas Front prontas

---
+++ ProjetoBancario/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBancario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+
+        public ActionResult Transferencia()
+        {
+            return View();
+        }
+
+        public ActionResult Saque()
+        {
+            return View();
+        }
+
+        public ActionResult Deposito()
+        {
+            return View();
+        }
+
+        public ActionResult Extrato()
+        {
+            return View();
+        }
+
+        public ActionResult Extorno()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoBancario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoBancario.MvcApplication" Language="C#" %>
